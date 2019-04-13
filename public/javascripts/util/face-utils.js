f06756e278048d1e1e32926d57970f9288199acf
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-04-13 21:39:39 +0300

    Fix bug of finding of different vertices in adjacent faces

--- public/javascripts/util/face-utils.js
+++ public/javascripts/util/face-utils.js
@@ -16,7 +16,7 @@ export const Faces = {
         for (let p of ['a', 'b', 'c']) {
             const idx = vertices.indexOf(face2[p]);
             if (idx < 0)
-                result.push(vertices.splice(idx, 1)[0]);
+                result.push(face2[p]);
         }
         return result;
     }
