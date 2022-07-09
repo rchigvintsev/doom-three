75d4b9dbe9fb10f6ad69d74a02796c191271552e
Roman Chigvintsev <rchigvintsev@gmail.com>
2022-07-09 13:23:29 +0300

    Add light source to flashlight

--- client/src/entity/md5model/weapon/flashlight.ts
+++ client/src/entity/md5model/weapon/flashlight.ts
@@ -1,17 +1,29 @@
-import {AnimationAction, Audio, BufferGeometry, Material, Object3D} from 'three';
+import {AnimationAction, Audio, BufferGeometry, Material, Object3D, SpotLight, Vector3} from 'three';
 
 import {randomInt} from 'mathjs';
 
 import {Weapon} from './weapon';
 import {GameConfig} from '../../../game-config';
 import {AttackEvent} from '../../../event/weapon-events';
+import {Player} from '../../player/player';
 
 const PUNCH_FORCE = 50;
 const ATTACK_DISTANCE = 30;
 
+const LIGHT_INTENSITY = 1.5;
+const LIGHT_DISTANCE = 1000;
+const LIGHT_ANGLE = Math.PI / 6;
+
 export class Flashlight extends Weapon {
     private readonly attackDistance!: number;
     private readonly punchAnimationActionNames = ['swing1', 'swing2'];
+
+    private readonly light?: SpotLight;
+    private readonly bone5Position = new Vector3();
+    private readonly bone6Position = new Vector3();
+    private readonly lightDirection = new Vector3();
+    private readonly lightPosition = new Vector3();
+    private readonly lightTargetPosition = new Vector3();
 
     private lastPunchAnimationAction?: AnimationAction;
 
@@ -20,7 +32,23 @@ export class Flashlight extends Weapon {
                 materials: Material | Material[],
                 sounds: Map<string, Audio<AudioNode>[]>) {
         super(config, geometry, materials, sounds);
+
         this.attackDistance = ATTACK_DISTANCE * config.worldScale;
+
+        if (!config.renderOnlyWireframe) {
+            this.light = new SpotLight();
+            this.light.intensity = LIGHT_INTENSITY;
+            this.light.distance = LIGHT_DISTANCE * config.worldScale;
+            this.light.angle = LIGHT_ANGLE;
+            this.add(this.light);
+            this.add(this.light.target);
+        }
+    }
+
+
+    update(deltaTime: number, player?: Player) {
+        super.update(deltaTime, player);
+        this.updateLight();
     }
 
     enable() {
@@ -55,6 +83,23 @@ export class Flashlight extends Weapon {
 
     onMiss(): void {
         this.playWooshSound();
+    }
+
+    private updateLight() {
+        if (this.light) {
+            this.updateMatrixWorld();
+
+            const bones = this.skeleton.bones;
+            this.bone5Position.setFromMatrixPosition(bones[5].matrixWorld);
+            this.bone6Position.setFromMatrixPosition(bones[6].matrixWorld);
+
+            this.lightDirection.subVectors(this.bone5Position, this.bone6Position).normalize();
+
+            this.light.position
+                .copy(this.worldToLocal(this.lightPosition.copy(this.bone5Position)));
+            this.light.target.position
+                .copy(this.worldToLocal(this.lightTargetPosition.copy(this.bone5Position).add(this.lightDirection)));
+        }
     }
 
     private canAttack() {
